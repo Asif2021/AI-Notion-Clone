dc468980c072d78ef0441b7b47a72e8b526ebe30
Asif <asiframzan2021@gmail.com>
2024-10-02 23:41:16 +0500

    Side Nav Started

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,23 @@
 "use client"
 
+import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/clerk-react";
 import { useUser } from "@clerk/nextjs"
 
 function Header() {
     const {user} = useUser();
   return (
-    <div>
-        {user && (<h1>{user?.firstName} {`'s`}  Space </h1> )}
+    <div className="flex items-center justify-between p-5">
+        {user && (<h1 className="text-2xl font-bold">{user?.firstName} {`'s`}  Space </h1> )}
 
 {/* Breadcrumbs */}
-
+<div>
+    <SignedOut>
+        <SignInButton/>
+    </SignedOut>
+    <SignedIn>
+        <UserButton/>
+    </SignedIn>
+</div>
     </div>
   )
 }
